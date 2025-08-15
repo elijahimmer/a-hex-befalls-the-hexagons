0c3845199b3e9ec4993dc998b7942045a53b36d5
Weeb-dev5 <kylenthefrog@gmail.com>
2025-08-15 12:46:15 -0700

    stuff so I don't stash

--- src/health_bar.rs
+++ src/health_bar.rs
@@ -9,6 +9,9 @@ pub struct HpPlugin;
 
 pub const HP_SPRITE_IMAGE_PATH: &str = "embedded://assets/sprites/HP-Sprite.png";
 pub const HP_BAR_IMAGE_PATH: &str = "embedded://assets/sprites/HP-Bar.png";
+pub const PRIESTESS_IMAGE_PATH: &str = "embedded://assets/sprites/Priestess_name.png";
+pub const THIEF_IMAGE_PATH: &str = "embedded://assets/sprites/thief_name.png";
+pub const WARRIOR_IMAGE_PATH: &str = "embedded://assets/sprites/Warrior_name.png";
 
 pub const FONT_SIZE: f32 = 18.0;
 pub const STANDARD_FLEX_GROW: f32 = 1.75;
@@ -17,6 +20,9 @@ impl Plugin for HpPlugin {
     fn build(&self, app: &mut App) {
         embed_asset!(app, "assets/sprites/HP-Sprite.png");
         embed_asset!(app, "assets/sprites/HP-Bar.png");
+        embed_asset!(app, "assets/sprites/Priestess_name.png");
+        embed_asset!(app, "assets/sprites/Thief_name.png");
+        embed_asset!(app, "assets/sprites/Warrior_name.png");
         app.add_systems(OnEnter(AppState::Game), create_hp_bars);
     }
 }
@@ -25,27 +31,32 @@ fn create_hp_bars(mut commands: Commands, style: Res<Style>, asset_server: Res<A
     // Left HP
     commands
         .spawn((Node {
-            flex_grow: 1.0,
-            flex_basis: Val::Px(5.0),
             align_items: AlignItems::Start,
             flex_direction: FlexDirection::Column,
-            justify_content: JustifyContent::Center,
             ..default()
         },))
         .with_children(|builder| {
             builder
                 .spawn(Node {
-                    flex_grow: 5.0,
-                    flex_basis: Val::Px(50.0),
                     align_items: AlignItems::Start,
                     flex_direction: FlexDirection::Row,
                     ..default()
                 })
                 .with_children(|builder| {
                     builder.spawn((
-                        Text2d::new("Warrior"),
-                        TextFont {
-                            font_size: FONT_SIZE,
+                        ImageNode {
+                            image: asset_server.load(WARRIOR_IMAGE_PATH),
+                            ..default()
+                        },
+                        Node {
+                            margin: UiRect::all(Val::Px(5.0)),
+                            ..default()
+                        },
+                    ));
+                    
+                    builder.spawn((
+                        ImageNode {
+                            image: asset_server.load(PRIESTESS_IMAGE_PATH),
                             ..default()
                         },
                         Node {
@@ -56,23 +67,9 @@ fn create_hp_bars(mut commands: Commands, style: Res<Style>, asset_server: Res<A
                         },
                     ));
                     builder.spawn((
-                        Text2d::new("Priestess"),
-                        TextFont {
-                            font_size: FONT_SIZE,
+                        ImageNode {
+                            image: asset_server.load(THIEF_IMAGE_PATH),
                             ..default()
-                        },
-                        Node {
-                            flex_grow: STANDARD_FLEX_GROW,
-                            flex_basis: Val::Px(100.0),
-                            margin: UiRect::all(Val::Px(5.0)),
-                            ..default()
-                        },
-                    ));
-                    builder.spawn((
-                        Text2d::new("Thief"),
-                        TextFont {
-                            font_size: FONT_SIZE,
-                            ..Default::default()
                         },
                         Node {
                             flex_grow: STANDARD_FLEX_GROW,
@@ -84,8 +81,6 @@ fn create_hp_bars(mut commands: Commands, style: Res<Style>, asset_server: Res<A
                 });
             builder
                 .spawn(Node {
-                    flex_grow: 5.0,
-                    flex_basis: Val::Px(50.0),
                     flex_direction: FlexDirection::Row,
                     align_items: AlignItems::Start,
                     ..default()
